3d5207046c342e1c176d21c290a2c9e4fc1ed764
Cliston507 <152149319+Cliston507@users.noreply.github.com>
2026-08-15 21:45:53 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import 'paywall_screen.dart';
 import 'package:memguard/memguard.dart';
 
-
+import 'paywall_screen.dart';
+import 'vault_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  await Purchases.configure(PurchasesConfiguration("test_XoCLIphRBnOHVkZeRVmnycNsX1W"));
 
-      final user = FirebaseAuth.instance.currentUser;
-if (user != null) {
-  await Purchases.logIn(user.uid);
-}
+  await Purchases.configure(
+    PurchasesConfiguration("test_XoCLiphRBnOHVkZeRVmnycNsX1W"),
+  );
 
-      
-
+  final user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    await Purchases.logIn(user.uid);
+  }
 
   runApp(const OmenVaultApp());
 }
@@ -30,25 +28,25 @@ class OmenVaultApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Omen Vault',
-      theme: ThemeData.dark(), 
-
-      home: const PaywallScreen(),
-
-   );
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
+      ),
+      home: const VaultScreen(),
     );
-            
+  }
+}
+
 class SecurityService {
-  final MemGuardCore _vault = MemGuardCore();
+  final MemGuard _memGuard = MemGuard();
 
   Future<void> secureStore({required String key, required String value}) async {
-    await _vault.write(key: key, value: value);
+    await _memGuard.write(key: key, value: value);
   }
 
   Future<String?> secureRead({required String key}) async {
-    return await _vault.read(key: key);
-  }
-
-  Future<void> secureWipe({required String key}) async {
-    await _vault.delete(key: key);
+    return await _memGuard.read(key: key);
   }
 }
